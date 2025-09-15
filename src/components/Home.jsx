907599e09f3edf3483e80f809f681898af473bea
Alexandre1422 <alexandre.coute@outlook.fr>
2025-09-15 22:08:28 +0200

    Mail add + test netlify

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -61,7 +61,7 @@ export default function Home({ lang = "en" }) {
 
       {/* Bouton mailto */}
       <a
-        href="mailto:tonadresse@mail.com"
+        href="mailto:alexandrecoute@outlook.fr"
         className="mt-6 inline-flex items-center gap-2 px-5 py-2 rounded-full bg-teal-400 text-white font-medium hover:bg-teal-500 transition-all"
       >
         <FaEnvelope className="text-lg" />
